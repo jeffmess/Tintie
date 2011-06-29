6075f00370bc3acea9ea9684975e6c025e89e714
Jeffrey van Aswegen <jeffmess@gmail.com>
2011-06-29 19:39:48 +0200

    Check for subclasses of Task.

--- lib/tintie/task.rb
+++ lib/tintie/task.rb
@@ -3,4 +3,14 @@ class Task < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
   belongs_to :create, :class_name => 'User'
   belongs_to :linkable, :polymorphic => true
+  
+  # Little hack to bypass autoloads
+  Dir[File.join(File.dirname(__FILE__),"/","*_task.rb")].each do |f|
+    Task.const_get(File.basename(f,'.rb').classify)
+  end
+  
+  # override deprecated method
+  def self.subclasses
+    direct_descendants.map(&:name)
+  end
 end
